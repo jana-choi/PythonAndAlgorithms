bd8ae4b2022183f567a1b5a20cd73c9587c563cc
jana-choi <lovej0918@gmail.com>
2020-07-05 18:14:12 +0900

    02. Built-in Sequence Types

---
+++ 02.Built-in Sequence Types/5_reversing_words.py
@@ -0,0 +1,22 @@
+def reverse_words_brute(string):
+    word, sentence = [], []
+    for character in string:
+        if character != " ":
+            word.append(character)
+        else:
+            if word:
+                sentence.append("".join(word))
+            word = []
+    
+    if word != "":
+        sentence.append("".join(word))
+    
+    sentence.reverse()
+    return " ".join(sentence)
+
+
+if __name__ == "__main__":
+    str1 = "파이썬 알고리즘 정말 재미있다"
+    str2 = reverse_words_brute(str1)
+    print(str1)
+    print(str2)
