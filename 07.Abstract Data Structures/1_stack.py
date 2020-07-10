fe27f3a47960790335a33d737d773190a8bd7710
jana-choi <lovej0918@gmail.com>
2020-07-10 17:05:25 +0900

    07. Abstract Data Structures (1)

---
+++ 07.Abstract Data Structures/1_stack.py
@@ -0,0 +1,46 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return not bool(self.items)
+    
+    def push(self, value):
+        self.items.append(value)
+    
+    def pop(self):
+        value = self.items.pop()
+        if value is not None:
+            return value
+        else:
+            print("Stack is empty.")
+    
+    def size(self):
+        return len(self.items)
+    
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            print("Stack is empty.")
+    
+    def __repr__(self):
+        return repr(self.items)
+
+
+if __name__ == "__main__":
+    stack = Stack()
+
+    print("스택이 비었나요? {}".format(stack.isEmpty()))
+    
+    print("스택에 숫자 0~9를 추가합니다.")
+    for i in range(10):
+        stack.push(i)
+    
+    print("스택 크기: {}".format(stack.size()))
+    print("peek: {}".format(stack.peek()))
+    print("pop: {}".format(stack.pop()))
+    print("peek: {}".format(stack.peek()))
+
+    print("스택이 비었나요? {}".format(stack.isEmpty()))
+    print(stack)
